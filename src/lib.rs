86bcc7dca1628091b0d31dcaaa8d0ee04aaba8d9
usrsem <andrey7menov@gmail.com>
2022-10-17 16:27:34 +0300

    init

---
+++ src/lib.rs
@@ -0,0 +1,39 @@
+#[derive(Debug, PartialEq, Clone)]
+pub struct ProgramInfo {
+    pub program_name: String,
+    pub window_title: String,
+    pub file_path: String,
+    pub file_name: String,
+    pub process_id: u32,
+    pub is_minimized: bool,
+    pub is_active: bool,
+}
+
+impl ProgramInfo {
+    pub fn new(program_name: &str, window_title: &str) -> Self {
+        Self {
+            program_name: String::from(program_name),
+            window_title: String::from(window_title),
+            file_name: String::new(),
+            file_path: String::new(),
+            process_id: 0,
+            is_minimized: false,
+            is_active: true,
+        }
+    }
+}
+
+pub enum DayOfWeek {
+    Monday,
+    Tuesday,
+    Wednesday,
+    Thursday,
+    Friday,
+    Saturday,
+    Sunday,
+}
+
+pub enum ProgramName {
+    Photoshop,
+    Sai,
+}
